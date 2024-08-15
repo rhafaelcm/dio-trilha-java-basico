dd77c9f87fe297b914d4eb2ec0e0efee13f5d493
Rhafael da Costa Martins <rhafaelcm@msn.com>
2024-08-15 10:22:25 -0400

    commit do projeto conta banco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Agora digite o numero da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Favor informar o nome:");
+        String nomeCliente = scanner.next(); //nextLine() está apresentando problema!
+
+        System.out.println("Informe o saldo da conta:");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
